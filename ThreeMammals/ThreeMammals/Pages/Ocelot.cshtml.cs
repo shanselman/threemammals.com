77f5a5a7674afeed68a364ceb15f557b54c3b9a6
TomPallister <TomPallister@users.noreply.github.com>
2018-03-01 16:00:01 +0000

    initial commit of website using some template

---
+++ ThreeMammals/ThreeMammals/Pages/Ocelot.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ThreeMammals.Pages
+{
+    public class OcelotModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
